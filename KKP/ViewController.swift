e0767ed1e75f3b13dbaa437db071737cdc8b0ab7
TT8 <tomasztacka@gmail.com>
2019-03-28 20:32:41 +0100

    Initial Commit

---
+++ KKP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KKP
+//
+//  Created by TT8 on 28/03/2019.
+//  Copyright © 2019 TT8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
